d01bd7db733ad0e958d0d3b93aebd41b9184928a
Diego Lizarraga <lizarragasanchezdiego@gmail.com>
2026-05-30 03:23:22 -0600

    correcciones en el iniciador de sistema

--- iniciar-bodega.bat
+++ iniciar-bodega.bat
@@ -2,8 +2,8 @@
 title Servidor Bodega PWA
 cd /d "%~dp0pwa-bodega"
 
-echo Liberando puerto 3002...
-for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":3002" ^| findstr "LISTENING" 2^>nul') do (
+echo Liberando puerto 3003...
+for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":3003" ^| findstr "LISTENING" 2^>nul') do (
     taskkill /PID %%a /F >nul 2>&1
 )
 
